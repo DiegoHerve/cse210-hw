adc1ed434e119746b6bda905b10da3d7b9a344d7
DiegoHerve <yandyhlorange@gmail.com>
2026-09-07 19:53:38 +0400

    exerc4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<double> numbers = new List<double>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            if (number == 0)
+            {
+                break;
+            }
+
+            numbers.Add(number);
+        }
+
+        double sum = 0;
+        double largest = numbers[0];
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum = sum + numbers[i];
+
+            if (numbers[i] > largest)
+            {
+                largest = numbers[i];
+            }
+        }
+
+        double average = sum / numbers.Count;
+
+        Console.WriteLine("The sum is: " + sum);
+        Console.WriteLine("The average is: " + average);
+        Console.WriteLine("The largest number is: " + largest);
     }
 }
+
+
+
+    
+
+
