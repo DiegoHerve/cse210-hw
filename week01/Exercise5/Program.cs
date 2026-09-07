a5607bb70948219c6763f6a1040dd82ee4ce32bf
DiegoHerve <yandyhlorange@gmail.com>
2026-09-08 00:05:09 +0400

    corrected

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -17,7 +17,7 @@ class Program
 
     static void DisplayWelcome()
     {
-        Console.WriteLine("Welcome to the program!");
+        Console.WriteLine( "Welcome to the program!");
     }
 
     static string PromptUserName()
