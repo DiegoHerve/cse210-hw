e653202a5196be45a9d5d09c628cdff541e07e00
DiegoHerve <yandyhlorange@gmail.com>
2026-09-07 14:47:30 +0400

    guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        Console.Write("What is your guess?: ");
+        guess = int.Parse(Console.ReadLine());
+    
+        if (guess > magicNumber)
+        {
+        Console.WriteLine("Lower");
+        }
+
+
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else if (guess == magicNumber)
+        {
+            Console.WriteLine("You guess it!");
+        }
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+
+        
+
+
+
+
+
     }
+
 }
